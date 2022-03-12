485df7f4d7dff3ff1d198be036059cf28ed39039
saidnuri <47485785+saidnuri@users.noreply.github.com>
2022-03-12 07:51:54 +0300

    Update index.py

--- index.py
+++ index.py
@@ -1,5 +1,7 @@
 from flask import Flask,request
 from pytube import YouTube
+from youtubesearchpython import VideosSearch
+import json
 
 app = Flask(__name__)
 
@@ -9,6 +11,12 @@ def index(name:str):
     yt = YouTube('http://youtube.com/watch?v='+str(name))
     file=yt.streams.filter(only_audio=True).first().url
     return file
+@app.route('/snc/<string:search>')
+def snc(search:str):
+    videosSearch = VideosSearch(search, limit = 50)
+    dict=videosSearch.result()
+    den=json.dumps(dict,indent=1)
+    return den
 
 if __name__ == '__main__':
     app.run()
