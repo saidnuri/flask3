dbdd7f2679b54768e65a4d5ad255a3550abf2505
saidnuri <47485785+saidnuri@users.noreply.github.com>
2022-04-16 18:34:39 +0300

    Update index.py

--- index.py
+++ index.py
@@ -8,17 +8,13 @@ app = Flask(__name__)
 
 @app.route('/<string:name>')
 def index(name:str):
-   url = "https://youtube-mp3-download1.p.rapidapi.com/dl"
-
+        url = "https://youtube-mp3-download1.p.rapidapi.com/dl"
 	querystring = {"id": name}
-
 	headers = {
 		"X-RapidAPI-Host": "youtube-mp3-download1.p.rapidapi.com",
 		"X-RapidAPI-Key": "529e3f03c0msh06af56ae5d4985dp153eeejsna77eac11e459"
 	}
-
 	response = requests.request("GET", url, headers=headers, params=querystring)
-
 	return (response.json()["link"])
 @app.route('/snc/<string:search>')
 def snc(search:str):
