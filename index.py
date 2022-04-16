bd296ca82910a6c22ddcb6138a820f54cc654e5a
saidnuri <47485785+saidnuri@users.noreply.github.com>
2022-04-16 18:58:08 +0300

    master

--- index.py
+++ index.py
@@ -1,4 +1,5 @@
-from flask import Flask,request
+from flask import Flask
+import requests
 from pytube import YouTube
 from youtubesearchpython import VideosSearch
 import json
@@ -6,16 +7,30 @@ import json
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/<string:name>')
 def index(name:str):
-     return "selam"
+    url = "https://youtube-mp3-download1.p.rapidapi.com/dl"
+    querystring = {"id": str(name)}
+    headers = {
+		"X-RapidAPI-Host": "youtube-mp3-download1.p.rapidapi.com",
+		"X-RapidAPI-Key": "529e3f03c0msh06af56ae5d4985dp153eeejsna77eac11e459"
+	}
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    return (response.json()["link"])
 @app.route('/snc/<string:search>')
 def snc(search:str):
     videosSearch = VideosSearch(search, limit = 50)
     dict=videosSearch.result()
     den=json.dumps(dict,indent=1)
     return den
+@app.route('/ytb_video/<string:id>')
+def get_video(id:str):
+    yt = YouTube('http://youtube.com/watch?v='+str(id))
+    linker = {"Linkler": [yt.streams.get_highest_resolution().url, yt.streams.get_lowest_resolution().url],
+              "kaliteler:": [yt.streams.get_highest_resolution().resolution,
+                             yt.streams.get_lowest_resolution().resolution]}
 
+    return linker
 
 if __name__ == '__main__':
     app.run()
